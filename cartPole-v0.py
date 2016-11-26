b85e8c78e5b178ed4280b20e5bd8f4b829e55ca0
Carsten Klein <cklein@ikf.uni-frankfurt.de>
2016-11-26 10:03:48 +0100

    clean up of code, same issue as before

--- cartPole-v0.py
+++ cartPole-v0.py
@@ -8,87 +8,50 @@ import matplotlib.pyplot as plt
 from collections import deque
 
 env = gym.make('CartPole-v0')
+# env = gym.make('CartPole-v1')
+# env = gym.make('Acrobot-v1')
 
 ### CONFIGURATION ###
-TIMESTEPS = 10000
-EPISODES = 1000
+TIMESTEPS = 2000
+EPISODES = 500
 
 ACTIONS = env.action_space.n
 INPUT_DIMENSION = env.observation_space.shape[0]
 OUTPUT_DIMENSION = ACTIONS
-HIDDEN_NEURONS1 = 20
-HIDDEN_NEURONS2 = 30
-LEARNING_RATE = 0.01
+HIDDEN_NEURONS1 = 16
+HIDDEN_NEURONS2 = 16
+LEARNING_RATE = 0.1
 DECAY_RATE = 0.1
-BATCH = 32
+BATCH = 64
 
 GAMMA = 0.99
 OBSERVE = 500
-INITIAL_EPSILON = 1.0
-FINAL_EPSILON = 0.1
 EXPLORE = 50.
-REPLAY_MEMORY = 10000
-K = 1 # only select an action every K frames
-
+INITIAL_EPSILON = 1.0
+FINAL_EPSILON = 0.01
+REPLAY_MEMORY = 1000
 
 ### MODEL ###
-with tf.name_scope('Model'):
-    with tf.name_scope('Model_Parameter'):
-        x = tf.placeholder("float", [None, INPUT_DIMENSION], "input")
+x = tf.placeholder("float", [None, INPUT_DIMENSION], "input")
+fc1 = tf.contrib.layers.fully_connected(x, num_outputs=HIDDEN_NEURONS1, activation_fn=tf.nn.relu,
+                                        weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                        biases_initializer=tf.constant_initializer(0.1))
+# fc2 = tf.contrib.layers.fully_connected(fc1, num_outputs=HIDDEN_NEURONS2, activation_fn=tf.nn.relu,
+#                                         weights_initializer=tf.contrib.layers.xavier_initializer(),
+#                                         biases_initializer=tf.constant_initializer(0.1))
+out_layer1 = tf.contrib.layers.fully_connected(fc1, num_outputs=OUTPUT_DIMENSION, activation_fn=None,
+                                        weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                        biases_initializer=tf.constant_initializer(0.1))
 
-    # # Construct model
-    # with tf.name_scope('NeuralNetwork'):
-    #     with tf.name_scope('Weights'):
-    #         weights = {
-    #         'h1': tf.get_variable(name="Hidden_Layer1", shape=[INPUT_DIMENSION, HIDDEN_NEURONS1], initializer=tf.contrib.layers.xavier_initializer()),
-    #         'h2': tf.get_variable(name="Hidden_Layer2", shape=[HIDDEN_NEURONS1, HIDDEN_NEURONS2], initializer=tf.contrib.layers.xavier_initializer()),
-    #         'out': tf.get_variable(name="Weight_out", shape=[HIDDEN_NEURONS2, OUTPUT_DIMENSION], initializer=tf.contrib.layers.xavier_initializer())
-    #         }
-    #     with tf.name_scope('Biases'):
-    #         biases = {
-    #         'b1':  tf.get_variable(name="bias_1", shape=[HIDDEN_NEURONS1], initializer=tf.random_normal_initializer()),
-    #         'b2':  tf.get_variable(name="bias_2", shape=[HIDDEN_NEURONS2], initializer=tf.random_normal_initializer()),
-    #         'out': tf.get_variable(name="bias_out", shape=[OUTPUT_DIMENSION], initializer=tf.random_normal_initializer())
-    #         }
-    #     # Hidden layer with RELU activation
-    #     with tf.name_scope('Layers'):
-    #         layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    #         layer_1 = tf.nn.relu(layer_1)
-    #         layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    #         layer_2 = tf.nn.relu(layer_2)
-    #         # Output layer with linear activation
-    #         out_layer2 = tf.matmul(layer_2, weights['out']) + biases['out']
-    # Construct model
-    with tf.name_scope('NeuralNetwork'):
-        with tf.name_scope('Weights'):
-            weights = {
-            'h1': tf.get_variable(name="Hidden_Layer1", shape=[INPUT_DIMENSION, HIDDEN_NEURONS1], initializer=tf.contrib.layers.xavier_initializer()),
-            'out': tf.get_variable(name="Weight_out", shape=[HIDDEN_NEURONS1, OUTPUT_DIMENSION], initializer=tf.contrib.layers.xavier_initializer())
-            }
-        with tf.name_scope('Biases'):
-            biases = {
-            'b1':  tf.get_variable(name="bias_1", shape=[HIDDEN_NEURONS1], initializer=tf.random_normal_initializer()),
-            'out': tf.get_variable(name="bias_out", shape=[OUTPUT_DIMENSION], initializer=tf.random_normal_initializer())
-            }
-        # Hidden layer with RELU activation
-        with tf.name_scope('Layers'):
-            layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-            layer_1 = tf.nn.relu(layer_1)
-            # Output layer with linear activation
-            out_layer1 = tf.matmul(layer_1, weights['out']) + biases['out']
+a = tf.placeholder("float", [None, ACTIONS], "actions")
+y_ = tf.placeholder("float", [None], "output")
 
-    a = tf.placeholder("float", [None, ACTIONS], "actions")
-    y_ = tf.placeholder("float", [None], "output")
+pred = out_layer1
 
-    # pred = out_layer2
-    pred = out_layer1
+readout_action = tf.reduce_sum(tf.mul(pred, a), reduction_indices = 1)
+cost = tf.contrib.losses.mean_squared_error(y_, readout_action)
 
-    readout_action = tf.reduce_sum(tf.mul(pred, a), reduction_indices = 1)
-    cost = tf.contrib.losses.mean_squared_error(y_, readout_action)
-    tf.scalar_summary('loss', cost)
-
-    with tf.name_scope('Optimizer'):
-        optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost)
 
 
 ### Preparation for TRAINING #####
@@ -107,22 +70,24 @@ with tf.Session() as sess:
     for i_episode in range(EPISODES):
         observation = env.reset()
         s_t = observation # Store first state in memory D
-
+        sum_reward = 0
         for t in range(TIMESTEPS):
-            # env.render()
+            # if t % BATCH == 0:
+            #     env.render()
 
             action = np.zeros([ACTIONS])
             if random.random() <= epsilon or t_total<=OBSERVE:
                 action_index = random.randrange(ACTIONS)
-                action[action_index] = 1
             else:
                 readout_t = pred.eval(feed_dict = {x : [s_t]})[0]
                 action_index = np.argmax(readout_t)
-                action[action_index] = 1
+            action[action_index] = 1
 
             # Get new state and append to buffer
             observation, reward, done, info = env.step(action_index)
             D.append((s_t, action, reward, observation, done))
+            sum_reward += reward
+
             if len(D) > REPLAY_MEMORY:
                 D.popleft()
 
@@ -143,7 +108,9 @@ with tf.Session() as sess:
                         y_batch.append(r_batch[i])
                     else:
                         y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
-                # print(y_batch)
+                # print("pred:           ", pred.eval(feed_dict={x: s_j_batch}))
+                # print("a :             ", a.eval(feed_dict={a: a_batch}))
+                # print("readout_action: ", readout_action.eval(feed_dict = {x: s_j_batch, a: a_batch}))
                 # perform gradient step
                 _c, c = sess.run([optimizer, cost], feed_dict = {
                     y_ : y_batch,
@@ -163,8 +130,8 @@ with tf.Session() as sess:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
         timestepsPerEpisode[0].append(i_episode)
-        timestepsPerEpisode[1].append(t+1)
-        print("eps", epsilon, "")
+        timestepsPerEpisode[1].append(sum_reward)
+        # print("eps", epsilon, "reward", sum_reward)
         # END OF EPISODE
 
 ## Plotting stuff
@@ -175,6 +142,6 @@ ax1.plot(timestepsPerEpisode[0], timestepsPerEpisode[1], color='blue')
 # plt.axis([-10., 10., -10., 10.])
 plt.title('cartPole-v0')
 plt.xlabel('episode')
-plt.ylabel('steps')
-plt.savefig('steps')
+plt.ylabel('reward')
+plt.savefig('reward')
 # env.monitor.close()
